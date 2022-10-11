5ac9f5443e602e72b1de344379fb95bc77ca9ac2
GaboMV <70921122+GaboMV@users.noreply.github.com>
2022-10-11 13:25:22 -0400

    Add files via upload

---
+++ practica/ms-customer/src/main/java/arquitectura/software/mscustomer/MsCustomerApplication.java
@@ -0,0 +1,17 @@
+package arquitectura.software.mscustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+public class MsCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerApplication.class, args);
+	}
+
+}
